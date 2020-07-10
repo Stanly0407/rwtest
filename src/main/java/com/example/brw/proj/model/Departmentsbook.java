fdbc9cec2c8d58da130054afb30dd9f54d7503ce
Rozenberg-jv <avangard.npaper@gmail.com>
2020-07-10 17:10:12 +0300

    fix

--- src/main/java/com/example/brw/proj/model/Departmentsbook.java
+++ src/main/java/com/example/brw/proj/model/Departmentsbook.java
@@ -4,44 +4,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @Entity
 public class Departmentsbook {
-    private long departmentId;
-    private long parentDepartmentId;
-    private String department;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "departmentID", nullable = false)
-    public long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    @Basic
-    @Column(name = "parentDepartmentID", nullable = false)
-    public long getParentDepartmentId() {
-        return parentDepartmentId;
-    }
-
-    public void setParentDepartmentId(long parentDepartmentId) {
-        this.parentDepartmentId = parentDepartmentId;
-    }
-
-    @Basic
-    @Column(name = "department", nullable = true, length = 255)
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "departmentID")
+	private long departmentId;
+	@Column(name = "parentDepartmentID", nullable = false)
+	private long parentDepartmentId;
+	@Column(name = "department")
+	private String department;
 }
--- src/main/java/com/example/brw/proj/model/Users.java
+++ src/main/java/com/example/brw/proj/model/Users.java
@@ -9,7 +9,12 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userID")
     private long userId;
+    @Column(name = "employeeID")
     private String employeeId;
     private String email;
     private String password;
@@ -17,141 +22,15 @@ public class Users {
     private String forename;
     private String patronym;
     private String position;
+    @Column(name = "`cellPhone`")
     private String cellPhone;
+    @Column(name = "officePhone")
     private String officePhone;
+    @Column(name = "homePhone")
     private String homePhone;
-    private byte enabled;
+    private boolean enabled;
+    @ManyToOne
+    @JoinColumn(name = "departmentID")
     private Departmentsbook departmentsbookByDepartmentId;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "userID", nullable = false)
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-
-    @Basic
-    @Column(name = "employeeID", nullable = false, length = 255)
-    public String getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    @Basic
-    @Column(name = "email", nullable = true, length = 255)
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Basic
-    @Column(name = "password", nullable = true, length = 255)
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Basic
-    @Column(name = "surname", nullable = true, length = 255)
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    @Basic
-    @Column(name = "forename", nullable = true, length = 255)
-    public String getForename() {
-        return forename;
-    }
-
-    public void setForename(String forename) {
-        this.forename = forename;
-    }
-
-    @Basic
-    @Column(name = "patronym", nullable = true, length = 255)
-    public String getPatronym() {
-        return patronym;
-    }
-
-    public void setPatronym(String patronym) {
-        this.patronym = patronym;
-    }
-
-    @Basic
-    @Column(name = "position", nullable = true, length = 255)
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    @Basic
-    @Column(name = "cellPhone", nullable = true, length = 255)
-    public String getCellPhone() {
-        return cellPhone;
-    }
-
-    public void setCellPhone(String cellPhone) {
-        this.cellPhone = cellPhone;
-    }
-
-    @Basic
-    @Column(name = "officePhone", nullable = true, length = 255)
-    public String getOfficePhone() {
-        return officePhone;
-    }
-
-    public void setOfficePhone(String officePhone) {
-        this.officePhone = officePhone;
-    }
-
-    @Basic
-    @Column(name = "homePhone", nullable = true, length = 255)
-    public String getHomePhone() {
-        return homePhone;
-    }
-
-    public void setHomePhone(String homePhone) {
-        this.homePhone = homePhone;
-    }
-
-    @Basic
-    @Column(name = "enabled", nullable = false)
-    public byte getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(byte enabled) {
-        this.enabled = enabled;
-    }
-
-
-    @ManyToOne
-    @JoinColumn(name = "departmentID", referencedColumnName = "departmentID")
-    public Departmentsbook getDepartmentsbookByDepartmentId() {
-        return departmentsbookByDepartmentId;
-    }
-
-    public void setDepartmentsbookByDepartmentId(Departmentsbook departmentsbookByDepartmentId) {
-        this.departmentsbookByDepartmentId = departmentsbookByDepartmentId;
-    }
 }
